df35151666a36977454554a56cf9728d23f68a5c
Dimitris Poulopoulos <dimpo@arrikto.com>
2022-09-22 12:58:53 +0300

    coronahack: Reconcile Coronack with the new PyTorch marshal backend
    Introduce changes to the Coronahack example due to the new PyTorch
    marshaling backend.
    
    The PyTorch marshaling backend creates an archive that includes the
    serialized model's weights and its definition (i.e., the Python module). Thus,
    it expects the definition of the model to reside in a different Python module,
    than the one defining the KFP steps so that it can reference it during the
    serialization process.
    
    When it loads the model, it first imports and executes the Python module
    to create the model object and then loads the weights.
    
    Refs arrikto/dev#1752
    Refs arrikto/dev#1889
    
    Signed-off-by: Dimitris Poulopoulos <dimpo@arrikto.com>
    Reviewed-by: George Alexopoulos <grgalex@arrikto.com>

---
+++ academy/coronahack-distributed-training/network.py
@@ -0,0 +1,28 @@
+
+# Copyright © 2022 Arrikto Inc.  All Rights Reserved.
+
+"""PyTorch Model Definition.
+
+This script defines a simple PyTorch CNN.
+"""
+
+import torch.nn as nn
+import torchvision.models as models
+
+
+class Net(nn.Module):
+    def __init__(self):
+        super(Net, self).__init__()
+        self.model = models.resnet18(pretrained=True)
+        self.model.conv1 = nn.Conv2d(
+            1, 64, kernel_size=(7, 7), stride=(2, 2),
+            padding=(3, 3), bias=False)
+
+        for param in self.model.parameters():
+            param.requires_grad = False
+
+        features_in = self.model.fc.in_features
+        self.model.fc = nn.Linear(features_in, 2)
+
+    def forward(self, x):
+        return self.model(x)
